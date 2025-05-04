c1e28dba64d5364fb8c4a5df7f366615a435a8eb
aseresti <aseresti@ryerson.ca>
2025-05-04 18:10:15 -0400

    Add Plot

--- CABG/MBFTools/CompareMorphology.py
+++ CABG/MBFTools/CompareMorphology.py
@@ -2,6 +2,8 @@ import os
 import vtk
 import argparse
 import numpy as np
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 from vtk.util.numpy_support import vtk_to_numpy
 from PrePostComparison import PrePostMBFMap
@@ -74,28 +76,53 @@ class CompareMorphology(PrePostMBFMap):
         MBF_Labels = super().ReadMBFLabels()
         WallThickness_data = dict()
         for key in MBF_Labels.keys():
-            WallThickness_data[key] = np.array([])
-            for i in MBF_Labels[key]:
-                territory_ = self.ThresholdInBetweenPoints(Surface, "TerritoryMaps", i, i+1)
-                wall_thickness = vtk_to_numpy(territory_.GetPointData().GetArray("TerritoryMaps"))
-                WallThickness_data[key] = np.append(wall_thickness, WallThickness_data[key])
+            if len(MBF_Labels[key]) > 0: 
+                WallThickness_data[key] = np.array([])
+                for i in MBF_Labels[key]:
+                    territory_ = self.ThresholdInBetweenPoints(Surface, "TerritoryMaps", i, i+1)
+                    wall_thickness = vtk_to_numpy(territory_.GetPointData().GetArray("Distance"))
+                    WallThickness_data[key] = np.append(wall_thickness, WallThickness_data[key])
 
         return WallThickness_data
 
-    def PlotResults(self):
-        pass
+    def PlotResults(self, data):
+        df = pd.DataFrame(data)
+        plt.figure(figsize=(8, 5))
+        sns.barplot(data=df, x="parameter", y="Value", hue="Time", palette="Set2")
+
+        plt.ylabel("Value (units vary)")
+        plt.xticks(rotation=45, ha="right")
+        plt.tight_layout()
+        plt.show()
 
     def main(self):
         Volume_A = self.ComputeVolume(self.CavityCapped_A)
         Volume_B = self.ComputeVolume(self.CavityCapped_B)
 
-        Surface_A = self.ComputeSurfaceArea(self.Endocardium_A)
-        Surface_B = self.ComputeSurfaceArea(self.Endocardium_B)
+        Surface_A_Endo = self.ComputeSurfaceArea(self.Endocardium_A)
+        Surface_B_Endo = self.ComputeSurfaceArea(self.Endocardium_B)
 
+        Surface_A_Epi = self.ComputeSurfaceArea(self.Epicardium_A)
+        Surface_B_Epi = self.ComputeSurfaceArea(self.Epicardium_B)
+
+        data = {"parameter":[], "Time":[], "Value":[]}
         print("Cavity of Volume- PreCABG: ", Volume_A)
         print("Cavity of Volume- PostCABG: ", Volume_B)
-        print("EndoCardium Surface- PreCABG: ", Surface_A)
-        print("EndoCardium Surface- PostCABG: ", Surface_B)
+        data["parameter"].extend(["Cavity Volume", "Cavity Volume"])
+        data["Time"].extend(["PreCABG (mL)", "PostCABG (mL)"])
+        data["Value"].extend([Volume_A, Volume_B])
+
+        print("EndoCardium Surface- PreCABG: ", Surface_A_Endo)
+        print("EndoCardium Surface- PostCABG: ", Surface_B_Endo)
+        data["parameter"].extend(["Endo Surface", "Endo Surface"])
+        data["Time"].extend(["PreCABG (cm^2)", "PostCABG (cm^2)"])
+        data["Value"].extend([Surface_A_Endo, Surface_B_Endo])
+
+        print("EndoCardium Surface- PreCABG: ", Surface_A_Epi)
+        print("EndoCardium Surface- PostCABG: ", Surface_B_Epi)
+        data["parameter"].extend(["Epi Surface", "Epi Surface"])
+        data["Time"].extend(["PreCABG (cm^2)", "PostCABG (cm^2)"])
+        data["Value"].extend([Surface_A_Epi, Surface_B_Epi])
 
         Epicardium_WT_A = self.ComputeWallThickness(self.Endocardium_A, self.Epicardium_A)
         Epicardium_WT_B = self.ComputeWallThickness(self.Endocardium_B, self.Epicardium_B)
@@ -104,6 +131,18 @@ class CompareMorphology(PrePostMBFMap):
 
         WriteVTPFile(self.output_surface_A, Epicardium_WT_Territory_A)
         WriteVTPFile(self.output_surface_B, Epicardium_WT_Territory_B)
+
+        WallThickness_Data_A = self.ExtractWallThicknessInTerritory(Epicardium_WT_Territory_A)
+        WallThickness_Data_B = self.ExtractWallThicknessInTerritory(Epicardium_WT_Territory_B)
+
+        for key in WallThickness_Data_A.keys():
+            print(key, np.mean(WallThickness_Data_A[key]), np.mean(WallThickness_Data_B[key]))
+            data["parameter"].extend([f"{key}_WallThickness", f"{key}_WallThickness"])
+            data["Time"].extend(["PreCABG (0.1mm)", "PostCABG (0.1)"])
+            data["Value"].extend([np.mean(WallThickness_Data_A[key])*100, np.mean(WallThickness_Data_B[key])*100])
+
+        #Epicardium_WT_Territory_A.GetPointData().GetArray("Distance")
+        self.PlotResults(data)
 
 
     def ThresholdInBetweenPoints(self, Surface, arrayname, value1, value2):
